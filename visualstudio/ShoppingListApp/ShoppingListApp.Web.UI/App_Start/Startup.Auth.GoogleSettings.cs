d927830ca1e2daa3934dbadc3ac7e8a18010a605
foufure <jonathan_orditz@hotmail.com>
2015-02-08 19:04:18 +0100

    [ADD] added missing google authentication file with IDs.

---
+++ visualstudio/ShoppingListApp/ShoppingListApp.Web.UI/App_Start/Startup.Auth.GoogleSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingListApp.Web.UI
+{
+    public partial class Startup
+    {
+        //Google Account: shoppinglistappharbor@gmail.com
+        private const string GoogleClientId = "143888277516-tn7pia64anes346ckbmd4uo1ep4dqeik.apps.googleusercontent.com";
+        private const string GoogleClientSecret = "62jGUM-EtoyjoRZZ9x6vMttD";
+    }
+}
