be766ff3d1d72b38065043ac8a2ea077b2771fa1
foufure <jonathan_orditz@hotmail.com>
2015-03-28 22:20:27 +0100

    [CHG] corrected accordion behavior for names with spaces and updated french items. [ADD] added additional trace information.

--- visualstudio/ShoppingListApp/ShoppingListApp.JobsScheduler/CronJobsScheduler.cs
+++ visualstudio/ShoppingListApp/ShoppingListApp.JobsScheduler/CronJobsScheduler.cs
@@ -5,29 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using Quartz;
 using Quartz.Impl;
+using NLog;
 
 namespace ShoppingListApp.JobsScheduler
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Cron", Justification = "Reviewed")]
     public static class CronJobsScheduler
     {
+        private static Logger cronStartLogger = LogManager.GetCurrentClassLogger();
+
         public static void InitializeJobScheduler()
         {
+            cronStartLogger.Trace("Start Initialization of Cron: " + DateTime.Now.ToString());
+
             // http://blog.appharbor.com/2012/4/18/scheduled-tasks-using-quartz-and-appharbor-background-workers
             var schedulerFactory = new StdSchedulerFactory();
             var scheduler = schedulerFactory.GetScheduler();
             scheduler.Start();
 
+            cronStartLogger.Trace("Cron Scheduler Initialized: " + DateTime.Now.ToString());
+
             var job = JobBuilder.Create<BackupAllJob>().Build();
+
+            cronStartLogger.Trace("Cron Job Created: " + DateTime.Now.ToString());
 
             // http://www.quartz-scheduler.org/documentation/quartz-1.x/tutorials/crontrigger
             var cron = TriggerBuilder.Create()
-                            //// .WithCronSchedule("0 * * ? * *") // Every minute
-                            .WithCronSchedule("0 0 23 ? * *") // Every day at 23:00
+                            .WithCronSchedule("0 * * ? * *") // Every minute
+                            //// .WithCronSchedule("0 0 23 ? * *") // Every day at 23:00
                             .StartAt(DateBuilder.FutureDate(1, IntervalUnit.Second)) // Next scheduled backup is configured ASAP after the service is up
                             .Build();
 
+            cronStartLogger.Trace("Cron Trigger Created: " + DateTime.Now.ToString());
+
             scheduler.ScheduleJob(job, cron);
+
+            cronStartLogger.Trace("Cron Scheduler Started: " + DateTime.Now.ToString());
         }
     }
 }
--- visualstudio/ShoppingListApp/ShoppingListApp.Web.UI/Global.asax.cs
+++ visualstudio/ShoppingListApp/ShoppingListApp.Web.UI/Global.asax.cs
@@ -5,17 +5,25 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using ShoppingListApp.I18N.Utils;
 using ShoppingListApp.JobsScheduler;
+using NLog;
 
 namespace ShoppingListApp.Web.UI
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private Logger AppStartLogger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AppStartLogger.Trace("Start App Cron JobScheduler: " + DateTime.Now.ToString());
+
             CronJobsScheduler.InitializeJobScheduler();
+
+            AppStartLogger.Trace("App Cron JobScheduler Started: " + DateTime.Now.ToString());
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1121:UseBuiltInTypeAlias", Justification = "Reviewed.")]
