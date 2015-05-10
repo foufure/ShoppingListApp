3c637de4e3f569edd5cb251fde6a9bb4740302c4
foufure <jonathan_orditz@hotmail.com>
2015-05-10 21:31:00 +0200

    [CHG] generated combined coverage for all the projects with one report generated in the root of the solution under coverage directory.

--- visualstudio/ShoppingListApp/ShoppingListApp.JobsScheduler.Test/coverage.bat
+++ visualstudio/ShoppingListApp/ShoppingListApp.JobsScheduler.Test/coverage.bat
@@ -1,15 +1,6 @@
 @ECHO OFF
-"..\..\..\packages\OpenCover.4.5.3723\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe" -targetargs:"ShoppingListApp.JobsScheduler.Test.dll /noshadow /nologo /framework:net-4.5.1" -register:"user" -mergebyhash
+"..\..\..\packages\OpenCover.4.5.3723\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe" -targetargs:"ShoppingListApp.JobsScheduler.Test.dll /noshadow /nologo /framework:net-4.5.1" -register:"user" -mergebyhash -mergeoutput -output:"..\..\..\coverage\ShoppingListApp.JobsScheduler.Test.xml"
 if %ERRORLEVEL% GEQ 1 GOTO ERROR
-
-REM DEL /F /Q ".\coverage\*.*"
-
-"..\..\..\packages\ReportGenerator.2.1.3.0\reportgenerator.exe" -reports:"results.xml" -targetdir:"coverage" Verbosity:"Error"
-if %ERRORLEVEL% GEQ 1 GOTO ERROR
-
-REM Firefox is causing the batch file to return an error code 1
-REM "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" ".\coverage\index.htm"
-exit 0
 
 :ERROR
 exit 0
