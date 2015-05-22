84501f3e04f082425c60f525f956a6622cd11e53
foufure <jonathan_orditz@hotmail.com>
2015-05-22 14:16:39 +0200

    [ADD] added the dummy ping job cs file missing.

---
+++ visualstudio/ShoppingListApp/ShoppingListApp.JobsScheduler/DummyPingJob.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Ionic.Zip;
+using NLog;
+using NLog.Interface;
+using Quartz;
+using ShoppingListApp.Domain.Abstract;
+
+namespace ShoppingListApp.JobsScheduler
+{
+    public class DummyPingJob : IJob
+    {
+        private ILogger dummyPingJobLogger;
+
+        public DummyPingJob(ILogger dummyPingJobLogger)
+        {
+            this.dummyPingJobLogger = dummyPingJobLogger;
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            // This is done to keep IIS alive, because after 20 minutes it shuts down on AppHarbor.
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadString("http://shoppinglistapp.apphb.com");
+            }
+        }
+    }
+}
